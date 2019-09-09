2a23276f8d4509a25703ffc77224a5675e34d1fc
Heorhii Siburov <g.siburov@gmail.com>
2019-09-09 15:09:51 -0700

    remove function is workable now

--- src/App.js
+++ src/App.js
@@ -18,10 +18,16 @@ function App() {
 		setCart([...cart, item]);
 	};
 
+	const removeItem = id => {
+		setCart(
+			cart.filter(item => item.id !== id)
+		)
+	}
+
 	return (
 		<div className="App">
 			<ProductContext.Provider value={{ products, addItem }}>
-				<CartContext.Provider value={{ cart }}>
+				<CartContext.Provider value={{ cart, removeItem }}>
 					<Navigation />
 
 					{/* Routes */}
