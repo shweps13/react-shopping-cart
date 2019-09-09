345095769f7d395615938d7f32fbc05915796068
Heorhii Siburov <g.siburov@gmail.com>
2019-09-09 15:43:36 -0700

    product switched to class component

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,19 +1,29 @@
 import React from 'react';
 
-const Product = props => {
+class Product extends React.Component {
+  
+render(props) {
+	
 	return (
 		<div className="product">
-			<img src={props.product.image} alt={`${props.product.title} book`} />
+			<img src={this.props.product.image} alt={`${this.props.product.title} book`} />
 
-			<h1 className="title">{props.product.title}</h1>
+			<h1 className="title">{this.props.product.title}</h1>
 
-			<p className="price">${props.product.price}</p>
+			<p className="price">${this.props.product.price}</p>
 
-			<button onClick={() => props.addItem(props.product)}>
+			<button onClick={() => this.props.addItem(this.props.product)}>
 				Add to cart
 			</button>
+
+			<button onClick={() => this.props.addItem(this.props.product)}>
+				Add to localStorage
+			</button>
+
 		</div>
 	);
 };
+
+}
 
 export default Product;
